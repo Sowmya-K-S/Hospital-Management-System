70f05f30381e1f6e168de608ad63502132878dd7
Sowmya-K-S <sowmya68380@gmail.com>
2024-01-02 20:26:27 +0530

    app names changed

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Doctor.views import *
+from Patient.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
