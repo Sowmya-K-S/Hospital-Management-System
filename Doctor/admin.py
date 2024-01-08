dc0581a90f3af05c1f8a4d796b9c61b3db015698
Sowmya-K-S <sowmya68380@gmail.com>
2024-01-08 13:23:11 +0530

    deleted

--- Doctor/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from Doctor.models import  Doctor_table,DepartmentTable
-
-
-# Register your models here.
-admin.site.register(Doctor_table)
-admin.site.register(DepartmentTable)
--- Patient/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from Patient.models import Patient,Appoint
-
-# Register your models here.
-admin.site.register(Patient)
-admin.site.register(Appoint)
